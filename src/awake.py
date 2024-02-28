b68b74d163d46ff188bbfd30ff2f33a87d616ced
Joji Antony <simula67@gmail.com>
2024-02-28 18:56:01 +0530

    Fix defects + set infinite wake as default

--- src/awake.py
+++ src/awake.py
@@ -2,20 +2,25 @@ import pyautogui
 import time
 import sys
 from datetime import datetime
+
 pyautogui.FAILSAFE = False
-numMin = None
-if ((len(sys.argv)<2) or sys.argv[1].isalpha() or int(sys.argv[1])<1):
-    numMin = 3
+minutes_to_keep_awake = None
+
+SLEEP_INTERVAL = 5
+
+if len(sys.argv) < 2 or sys.argv[1].isalpha() or int(sys.argv[1]) < 1:
+    minutes_to_keep_awake = float("inf")
 else:
-    numMin = int(sys.argv[1])
-while(True):
-    x=0
-    while(x<numMin):
-        time.sleep(60)
-        x+=1
-    for i in range(0,200):
-        pyautogui.moveTo(0,i*4)
-    pyautogui.moveTo(1,1)
-    for i in range(0,3):
-        pyautogui.press("shift")
-    print("Movement made at {}".format(datetime.now().time()))
+    minutes_to_keep_awake = int(sys.argv[1])
+
+while True:
+    x = 0
+    while x < (minutes_to_keep_awake * 60):
+        time.sleep(SLEEP_INTERVAL)
+        x += SLEEP_INTERVAL
+        for i in range(0, 200):
+            pyautogui.moveTo(0, i * 4)
+        pyautogui.moveTo(1, 1)
+        for i in range(0, 3):
+            pyautogui.press("shift")
+        print("Movement made at {}".format(datetime.now().time()))
